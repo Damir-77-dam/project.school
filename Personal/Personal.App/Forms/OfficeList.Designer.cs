af98e5d760a8206f9d1e7ccacfda5ebcbb4c0078
GothicSecret <GothicSecret@gmail.com>
2025-09-13 00:10:08 +0200

    --

--- Personal/Personal.App/Forms/OfficeList.Designer.cs
+++ Personal/Personal.App/Forms/OfficeList.Designer.cs
@@ -62,7 +62,7 @@
             // 
             // button1
             // 
-            button1.Location = new Point(64, 37);
+            button1.Location = new Point(47, 35);
             button1.Name = "button1";
             button1.Size = new Size(75, 23);
             button1.TabIndex = 1;
